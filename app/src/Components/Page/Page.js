9d16020a839aaa8fa36e01efb84b2f299e0bdf7b
David Yu <yu.davidcheng@gmail.com>
2023-11-01 23:47:41 +0100

    Add footer and css

--- app/src/Components/Page/Page.js
+++ app/src/Components/Page/Page.js
@@ -1,8 +1,11 @@
+import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
+import './Page.css';
 
 export default function Page({children}) {
-  return <div>
+  return <div className="container">
     <NavBar></NavBar>
     {children}
+    <Footer></Footer>
   </div>
 }
